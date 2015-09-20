8dcb1b53daeb7727063ff7e352440ecedca24ef6
Oisin Hurley <oisin.hurley@gmail.com>
2015-09-20 20:36:35 +0100

    Update to specify region to investigate and to include operating services

--- lib/wta.rb
+++ lib/wta.rb
@@ -5,21 +5,35 @@ module Wta
   RED   = 3
   YELLA = 2
   INFO  = 1
+  FINE  = 0
 
-  def self.reds
-    bo_selecta RED
+  NA="NA"
+  SA="SA"
+  EU="EU"
+  AP="AP"
+
+  def self.regions
+    [NA, SA, EU, AP]
   end
 
-  def self.yellows
-    bo_selecta YELLA
+  def self.reds(region=NA)
+    bo_selecta(RED, region)
   end
 
-  def self.infos
-    bo_selecta INFO
+  def self.yellows(region=NA)
+    bo_selecta(YELLA, region)
   end
 
-  def self.bo_selecta(statusnum)
-    pull_statuses
+  def self.infos(region=NA)
+    bo_selecta(INFO, region)
+  end
+
+  def self.operating(region=NA)
+    bo_selecta(FINE, region)
+  end
+
+  def self.bo_selecta(statusnum, region)
+    pull_statuses(region)
     @all_status_images.select { |img|
       img.attributes['src'].value == "/images/status#{statusnum}.gif"
     }.map { |img|
@@ -27,8 +41,8 @@ module Wta
     }
   end
 
-  def self.pull_statuses
+  def self.pull_statuses(region)
     @html = Nokogiri::HTML(open(URI.parse('http://status.aws.amazon.com/')))
-    @all_status_images = @html.css('#current_events_block td img')
+    @all_status_images = @html.css('#current_events_block #' + region.upcase + '_block td img')
   end
 end
